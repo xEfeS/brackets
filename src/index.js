3db8c172c52dc0128b7c4dd75d4d260813bd3c15
xEfeS <32041360+xEfeS@users.noreply.github.com>
2020-10-19 22:43:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   let obj ={};
   for(let i=0; i < bracketsConfig.length; i++) {
-      obj[bracketsConfig[i][1]] === bracketsConfig[i][0];
+      obj[bracketsConfig[i][1]] = bracketsConfig[i][0];
   }
   let arr = str.split('');
   for(let i=0; i<str.length;i++) {
